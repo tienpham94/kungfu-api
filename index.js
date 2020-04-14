ae55a04bc67dd70014b32e24f1092d96147a266b
Tien Pham <tiendev94@gmail.com>
2020-04-14 20:57:32 +0300

    NonNull

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const { ApolloServer, gql } = require("apollo-server");
 
 const typeDefs = gql`
+  enum Status {
+    WATCHED
+    INTERESTED
+    NOT_INTERESTED
+    UNKNOWN
+  }
+
+  type Actor {
+    id: ID!
+    name: String!
+  }
+
   type Movie {
+    id: ID!
     title: String
     releaseDate: String
     rating: Int
+    status: Status
+    actor: [Actor]
     # FKiat, Boolean
   }
 
@@ -15,7 +30,12 @@ const typeDefs = gql`
 
 const movies = [
   { title: "5 Deadly Venoms", releaseDate: "10-10-1983", rating: 5 },
-  { title: "36th Chamber", releaseDate: "10-10-1983", rating: 5 }
+  {
+    title: "36th Chamber",
+    releaseDate: "10-10-1983",
+    rating: 5,
+    actor: [{ id: "asfasfd", name: "Gordon Liu" }]
+  }
 ];
 
 const resolvers = {
